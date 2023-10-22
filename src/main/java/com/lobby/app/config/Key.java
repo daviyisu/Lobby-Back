f7d67d52d3dfd5dd7d221581a160f252ff056aff
daviyisu <davidjrvt@hotmail.com>
2023-10-22 11:45:44 +0200

    status

---
+++ src/main/java/com/lobby/app/config/Key.java
@@ -0,0 +1,8 @@
+package com.lobby.app.config;
+
+public class Key {
+    public static String steamApiKey = "6F6D8D63CCEA7CC3713675E7A2A9FF3B";
+    public static String clientId = "uzkg42ck2gorbxho0fpmc01esr6dbl";
+    public static String secretId = "keewysnbdndp4d418cwuwv3wqy8t1y";
+    public static String accessToken = "99l775jcx59d99xuclldh1l37f6h3r";
+}
