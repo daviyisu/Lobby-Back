e352666a35576a1d633a9862c2250a84992061e7
daviyisu <davidjrvt@hotmail.com>
2023-11-23 20:18:27 +0100

    Add issue change

--- src/main/java/com/lobby/app/controller/GameController.java
+++ src/main/java/com/lobby/app/controller/GameController.java
@@ -11,15 +11,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
 
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
 
 @Setter
 @Getter
@@ -33,13 +36,13 @@ public class GameController {
 
     private final UserRepository userRepository;
 
-    private final String steamApiBase = "https://api.steampowered.com";
+    private static final String STEAM_API_BASE = "https://api.steampowered.com";
 
-    private final String igdbApiBase = "https://api.igdb.com/v4/";
+    private static final String IGDB_API_BASE = "https://api.igdb.com/v4/";
 
-    private final String steamUrlBase = "https://store.steampowered.com/app/";
+    private static final String STEAM_URL_BASE = "https://store.steampowered.com/app/";
 
-    private final String steamApiKey = Key.steamApiKey;
+    private static final String STEAM_API_KEY = Key.steamApiKey;
 
     private final WebClient.Builder webClientBuilder;
 
@@ -61,15 +64,15 @@ public class GameController {
         assert webClientBuilder != null;
         String jsonResponse = webClientBuilder.build()
                 .get()
-                .uri(this.steamApiBase + "/IPlayerService/GetOwnedGames/v0001/?key="
-                        + this.steamApiKey
-                        + "&steamid="+userSteamId
-                        +"&include_appinfo=true&format=json")
+                .uri(STEAM_API_BASE + "/IPlayerService/GetOwnedGames/v0001/?key="
+                        + STEAM_API_KEY
+                        + "&steamid=" + userSteamId
+                        + "&include_appinfo=true&format=json")
                 .retrieve().bodyToMono(String.class).block();
         JsonNode jsonNode = objectMapper.readTree(jsonResponse);
         List<Integer> result = new ArrayList<>();
         JsonNode gamesArray = jsonNode.get("response").get("games");
-        for (int i=0; i<gamesArray.size(); i++) {
+        for (int i = 0; i < gamesArray.size(); i++) {
             result.add(gamesArray.get(i).get("appid").asInt());
         }
         return result;
@@ -81,23 +84,29 @@ public class GameController {
     private List<Integer> getIgdbGamesIdsFromSteam(List<Integer> steamAppIds) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
         assert webClientBuilder != null;
-        StringBuilder requestBody = new StringBuilder("fields game; where url=\"https://store.steampowered.com/app/" + steamAppIds.get(0).toString() + "\"");
-        if(steamAppIds.size() > 1) {
-            for (int i=1; i<steamAppIds.size(); i++) {
-                requestBody.append(" | url=\"https://store.steampowered.com/app/").append(steamAppIds.get(i).toString()).append("\"");
+        StringBuilder requestBody = new StringBuilder().append("fields game; where url=\"")
+                .append(STEAM_URL_BASE)
+                .append(steamAppIds.get(0).toString())
+                .append("\"");
+        if (steamAppIds.size() > 1) {
+            for (int i = 1; i < steamAppIds.size(); i++) {
+                requestBody.append(" | url=\"")
+                        .append(STEAM_URL_BASE)
+                        .append(steamAppIds.get(i).toString())
+                        .append("\"");
             }
         }
-       requestBody.append("; limit 500;");
+        requestBody.append("; limit 500;");
         String jsonResponse = webClientBuilder.build()
                 .post()
-                .uri(this.igdbApiBase + "/websites")
+                .uri(IGDB_API_BASE + "/websites")
                 .header("Client-ID", Key.clientId)
                 .header("Authorization", "Bearer " + Key.accessToken)
                 .body(BodyInserters.fromValue(requestBody.toString()))
                 .retrieve().bodyToMono(String.class).block();
         JsonNode jsonNode = objectMapper.readTree(jsonResponse);
         List<Integer> result = new ArrayList<>();
-        for(int i=0; i<jsonNode.size(); i++) {
+        for (int i = 0; i < jsonNode.size(); i++) {
             result.add(jsonNode.get(i).get("game").asInt());
         }
         return result;
@@ -109,14 +118,14 @@ public class GameController {
         Optional<Game> optionalGame;
         assert gameRepository != null;
         optionalGame = gameRepository.findById(id);
-        if (optionalGame.isPresent()){
+        if (optionalGame.isPresent()) {
             game_to_return = optionalGame.get();
         }
         return game_to_return;
     }
 
     @PostMapping("/addgame")
-    public ResponseEntity<Game> addUser(@RequestBody Game newGame){
+    public ResponseEntity<Game> addUser(@RequestBody Game newGame) {
         assert this.gameRepository != null;
         this.gameRepository.save(newGame);
         return ResponseEntity.ok(newGame);
@@ -130,6 +139,26 @@ public class GameController {
         List<Integer> steamAppIds = this.getSteamAppIds(userSteamId);
         return this.getIgdbGamesIdsFromSteam(steamAppIds);
     }
+
+//    @GetMapping("/getimage/{gameId}")
+//    public String getCoverId(@PathVariable Long gameId) throws JsonProcessingException {
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        assert webClientBuilder != null;
+//        String requestBody = "fields image_id; where game=" + gameId + ";" + " limit 500;";
+//
+//        String jsonResponse = webClientBuilder.build()
+//                .post()
+//                .uri(IGDB_API_BASE + "/covers")
+//                .header("Client-ID", Key.clientId)
+//                .header("Authorization", "Bearer " + Key.accessToken)
+//                .body(BodyInserters.fromValue(requestBody))
+//                .retrieve()
+//                .bodyToMono(String.class)
+//                .block();
+//        JsonNode jsonNode = objectMapper.readTree(jsonResponse);
+//
+//        return "https://images.igdb.com/igdb/image/upload/t_1080p/" + jsonNode.get(0).get("image_id").asText() + ".jpg";
+//    }
 
 
     /*
